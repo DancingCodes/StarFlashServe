6afe2a08546641bf2c28da4af777cfd526863ec8
张长悦 <dancingcodes@qq.com>
2024-02-05 11:46:40 +0800

    1

--- serve.js
+++ serve.js
@@ -1,8 +1,26 @@
 // 引入express
 const express = require('express');
-
 // 创建web服务器
-const serve = express();
+const expressServe = express();
+let serve = null
+if (process.env.NODE_ENV === 'development') {
+    // 本地
+    const http = require('http')
+    serve = http.createServer(expressServe).listen(require('./public/prot'), () => {
+        console.log('服务器启动成功啦( ≖ ◡ ≖✿ )')
+    });
+} else {
+    // 线上
+    const https = require('https')
+    serve = https.createServer({
+        key: fs.readFileSync('./static/ssl/api.moonc.love.key'),
+        cert: fs.readFileSync('./static/ssl/api.moonc.love.crt')
+    }, expressServe).listen(require('./public/prot'), () => {
+        console.log('服务器启动成功啦( ≖ ◡ ≖✿ )')
+    });
+}
+
+
 module.exports = serve
 
 // 链接数据库
@@ -13,8 +31,3 @@ require('./router')
 
 // 加载工具
 require('./utils')
-
-// 监听端口号
-serve.listen(require('./public/prot'), () => {
-    console.log('服务器启动成功啦( ≖ ◡ ≖✿ )')
-})
