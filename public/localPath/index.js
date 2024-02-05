d9d8ca3ddc38c67b446ab506cfe52067b1b5cd77
张长悦 <dancingcodes@qq.com>
2024-02-05 12:05:58 +0800

    1

--- public/localPath/index.js
+++ public/localPath/index.js
@@ -5,7 +5,7 @@ if (process.env.NODE_ENV === 'development') {
     localPath = `http://localhost:${require('../prot')}`
 } else {
     // 线上
-    localPath = `http://api.moonc.love:${require('../prot')}`
+    localPath = `https://api.moonc.love:${require('../prot')}`
 }
 
 module.exports = localPath
